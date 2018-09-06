454bcad17fd0b8b90590f80fe6c36a950fda66ff
USER <USER@asus.mshome.net>
2018-09-06 10:41:06 +0700

    Jobsheet5

---
+++ src/Praktikum/Catch.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktikum;
+
+/**
+ *
+ * @author USER
+ */
+public class Catch {
+    public static void main(String[] args) {
+        try
+        {
+            int a = 1/0; 
+            System.out.println("perintah selanjutnya");
+        }
+        catch(NullPointerException e)
+        {
+        }
+        catch(ArrayIndexOutOfBoundsException e)
+        {
+        }
+        catch(Exception e)
+        {
+        }
+    }
+}
+
---
+++ src/Praktikum/Try.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktikum;
+
+/**
+ *
+ * @author USER
+ */
+public class Try {
+     public static void main(String [] args){
+      try {
+        int a = 1 / 0; 
+        System.out.println("perintah selanjutnya");  
+      }
+      catch (Exception kesalahan){
+          System.err.println(kesalahan);
+      }
+   } 
+}
+
